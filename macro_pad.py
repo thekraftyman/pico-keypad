06e2140841e2a506e931f8df3a1463a6e4cfa415
thekraftyman <thekraftyman@gmail.com>
2022-06-15 08:11:25 -0500

    fix test 2, it works now

--- macro_pad.py
+++ macro_pad.py
@@ -1,82 +1,79 @@
 # macro_pad.py
 
 from pad import Keypad
-from types import FunctionType
 
 class MacroPad( Keypad ):
-	def __init__( self, **kwargs ):
-		''' macro pad based on the pico keypad '''
-		super().__init__( **kwargs )
-		self._bindings = {}
+    def __init__( self, **kwargs ):
+        ''' macro pad based on the pico keypad '''
+        super().__init__( **kwargs )
+        self._bindings = {}
 
-	@property
-	def bindings( self ):
-		''' dictionary where key bindings are defined '''
-		return self._bindings
+    @property
+    def bindings( self ):
+        ''' dictionary where key bindings are defined '''
+        return self._bindings
 
-	@property
-	def bound_pressed_buttons( self ):
-		''' returns a list of buttons that are both pressed and bound to something '''
-		return [ int(key) for key in self._pressed_buttons if self.is_bound( key ) ]
+    @property
+    def bound_pressed_buttons( self ):
+        ''' returns a list of buttons that are both pressed and bound to something '''
+        return [ int(key) for key in self.pressed_buttons if self.is_bound( key ) ]
 
-	def bind_key( self, key_num, callback, color=None ):
-		'''
-		Binds a callback function a key to run when the key is pressed
+    def bind_key( self, key_num, callback, color=None ):
+        '''
+        Binds a callback function a key to run when the key is pressed
 
-		:arg int key_num: key int val to bind
-		:arg FunctionType callback: function to call when the key is pressed
-		:arg tuple color: a tuple with 3 integers denoting the color of the
-			button (r,g,b) from [0-255]
-		'''
-		# do some error checking
-		assert type( callback ) == FunctionType
-		key_num = int( key_num )
-		self.check_key( key_num )
-		if key_num in self._bindings:
-			raise Exception( f"Key {key_num} is already bound to a function. Use the `drop_key` function to release the binding before rebinding the key" )
+        :arg int key_num: key int val to bind
+        :arg FunctionType callback: function to call when the key is pressed
+        :arg tuple color: a tuple with 3 integers denoting the color of the
+            button (r,g,b) from [0-255]
+        '''
+        # do some error checking
+        key_num = int( key_num )
+        self.check_key( key_num )
+        if key_num in self._bindings:
+            raise Exception( f"Key {key_num} is already bound to a function. Use the `drop_key` function to release the binding before rebinding the key" )
 
-		# bind the key
-		self._bindings[ key_num ] = callback
+        # bind the key
+        self._bindings[ key_num ] = callback
 
-		# set the color
-		if color:
-			r, g, b = color
-			self.set_color( key_num, r, g, b )
+        # set the color
+        if color:
+            self.set_color( key_num, color )
 
-	def call( self, key_num, **kwargs ):
-		'''
-		call a function from the bindings
+    def call( self, key_num, **kwargs ):
+        '''
+        call a function from the bindings
 
-		:arg int key_num: key int val to call
-		'''
-		# do some error checking
-		key_num = int( key_num )
-		self.check_key( key_num )
+        :arg int key_num: key int val to call
+        '''
+        # do some error checking
+        key_num = int( key_num )
+        self.check_key( key_num )
 
-		# run the function
-		self._bindings[ key_num ]( **kwargs )
+        # run the function
+        self._bindings[ key_num ]( **kwargs )
 
-	def drop_key( self, key_num ):
-		'''
-		Remove binding on a given key
+    def drop_key( self, key_num ):
+        '''
+        Remove binding on a given key
 
-		:arg int key_num: key int val to drop from bindings
-		'''
-		# do some error checking
-		key_num = int( key_num )
-		self.check_key( key_num )
-		if key_num not in self._bindings:
-			return
+        :arg int key_num: key int val to drop from bindings
+        '''
+        # do some error checking
+        key_num = int( key_num )
+        self.check_key( key_num )
+        if key_num not in self._bindings:
+            return
 
-		# remove the bound function
-		func = self._bindings.pop( key_num )
+        # remove the bound function
+        func = self._bindings.pop( key_num )
 
-	def is_bound( self, key_num ):
-		'''
-		checks to see if a key is bound
+    def is_bound( self, key_num ):
+        '''
+        checks to see if a key is bound
 
-		:arg int key_num: key int val to check for binding
+        :arg int key_num: key int val to check for binding
 
-		@return bool
-		'''
-		return int(key_num) in self._bindings
+        @return bool
+        '''
+        return int(key_num) in self._bindings
--- test-2.py
+++ test-2.py
@@ -5,49 +5,49 @@ from macro_pad import MacroPad
 from time import sleep
 
 def main():
-	# init the macro pad
-	pad = MacroPad()
+    # init the macro pad
+    pad = MacroPad()
 
-	# bind some keys
-	pad.bind_key( 0, say_hello, color=(100,0,0) )
-	pad.bind_key( 1, say_hello, color=(0,0,100) )
+    # bind some keys
+    pad.bind_key( 0, say_hello, color=[100,0,0] )
+    pad.bind_key( 1, say_hello, color=[0,0,100] )
 
-	# add some options
-	options = {
-		1 : {
-			'kwargs' : { 'name' : 'thekraftyman' },
-			'held' : False
-		}
-	}
+    # add some options
+    options = {
+        1 : {
+            'kwargs' : { 'name' : 'thekraftyman' },
+            'held' : False
+        }
+    }
 
-	# run the loop
-	while True:
-		# see if any buttons are being pressed
-		if not pad.pressed:
-			sleep( 0.1 )
-			continue
+    # run the loop
+    while True:
+        # see if any buttons are being pressed
+        if not pad.pressed:
+            sleep( 0.1 )
+            continue
 
-		# iterate through pressed buttons
-		for button in pad.bound_pressed_buttons:
-			if button in options and 'kwargs' in options[button]:
+        # iterate through pressed buttons
+        for button in pad.bound_pressed_buttons:
+            if button in options and 'kwargs' in options[button]:
                 if 'held' in options[button] and options[button]['held']:
-					pad.call( button, **options[button]['kwargs'] )
-					while button in pad.bound_pressed_buttons:
-						sleep(0.05)
-				else:
-					pad.call( button, **options[button]['kwargs'] )
-			else:
-				pad.call( button )
+                    pad.call( button, **options[button]['kwargs'] )
+                    while button in pad.bound_pressed_buttons:
+                        sleep(0.05)
+                else:
+                    pad.call( button, **options[button]['kwargs'] )
+            else:
+                pad.call( button )
 
-		# sleep to make sure we don't run the button too many times
-		sleep( 0.5 )
+        # sleep to make sure we don't run the button too many times
+        sleep( 0.5 )
 
 def say_hello( name=None ):
-	tosay = "Hello"
-	if name:
-		tosay = tosay + f" {name}"
-	tosay = tosay + ", how are you?"
-	print( tosay )
+    tosay = "Hello"
+    if name:
+        tosay = tosay + f" {name}"
+    tosay = tosay + ", how are you?"
+    print( tosay )
 
 if __name__ == "__main__":
-	main()
+    main()
